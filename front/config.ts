f53e2d7f5dfe1f19f2c78903e3635397c0d89645
theomhn <theo.menchon@hotmail.fr>
2025-03-13 22:47:35 +0100

    API de PROD / DEV

--- front/config.ts
+++ front/config.ts
@@ -1,2 +1,7 @@
 // Configuration globale de l'application
-export const API_BASE_URL = "http://127.0.0.1:8000/api";
+
+const LOCAL_API = "http://127.0.0.1:8000/api";
+
+const PROD_API = "http://coachzone.theomenchon.fr/api";
+
+export const API_BASE_URL = LOCAL_API;
